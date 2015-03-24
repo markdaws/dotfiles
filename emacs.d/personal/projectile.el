cc88088efdaccaaa43054c14c6cf9619cb2c1c31
Mark <mdawson@salesforce.com>
2015-03-24 09:20:33 -0700

    Turn on native indexing on projectile

--- emacs.d/personal/projectile.el
+++ emacs.d/personal/projectile.el
@@ -1,3 +1,7 @@
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
+
+(setq projectile-indexing-method 'native)
+(setq projectile-enable-caching t)
+(setq helm-projectile-fuzzy-match t)
 ;;(helm-projectile-on)
