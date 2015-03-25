e9e0ef6aa8e5e96dc45dfec0572ce9da70c23bdc
Mark Dawson <mdawson@salesforce.com>
2015-03-25 08:25:06 -0700

    Fix emacs C-x C-m command

--- emacs.d/personal/bindings.el
+++ emacs.d/personal/bindings.el
@@ -46,7 +46,7 @@
 (global-set-key (kbd "M-K") 'kill-this-buffer)
 
 ;; Easier M-x invocation
-(global-set-key "\C-x\C-m" 'exceute-extended-command)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
 ;; Improved navigation and editing (assumes misc.el)
