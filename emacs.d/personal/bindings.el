97b71d1a53d657c41e55eff5ce7ca48e1538881f
Mark <mdawson@salesforce.com>
2015-03-25 09:08:23 -0700

    Fix new meta binding - need more coffee

--- emacs.d/personal/bindings.el
+++ emacs.d/personal/bindings.el
@@ -47,6 +47,7 @@
 
 ;; Easier M-x invocation
 (global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
 
 ;; Faster word kill
 (global-set-key "\C-c\C-c" 'backward-kill-word)
