5efc05f6332514e995c47130537ab96bda247e7c
Mark <mdawson@salesforce.com>
2015-04-22 13:43:19 -0700

    emacs: modify helm-locate locatedb path

--- emacs.d/personal/helm.el
+++ emacs.d/personal/helm.el
@@ -4,7 +4,7 @@
 ;;(setq helm-locate-fuzzy-match t)
 (setq helm-idle-delay 0.1)
 (setq helm-input-idle-delay 0.15)
-(setq helm-locate-command "locate -d /home/mdawson/blt/app/196/patch/core/mylocatedb %s %s")
+(setq helm-locate-command "locate -d /home/mdawson/blt/app/main/core/mylocatedb %s %s")
 
 ;; Note to generate a localdb used the following command
-;; updatedb -l 0 -U . -o ./mylocatedb --prunepaths "`find /home/mdawson/blt/app/196/patch/core -regextype posix-extended -type d -iregex ".*/(target|tmp-cache|build)" -printf "%p "`" && locate -d ./mylocatedb search.cmp
+;; updatedb -l 0 -U . -o ./mylocatedb --prunepaths "`find /home/mdawson/blt/app/main/core -regextype posix-extended -type d -iregex ".*/(target|tmp-cache|build)" -printf "%p "`" && locate -d ./mylocatedb search.cmp
